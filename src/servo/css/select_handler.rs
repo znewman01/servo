6c545dbfbe439cf01167fa16827ec21f9e20f513
Brian Anderson <banderson@mozilla.com>
2012-11-02 18:57:30 -0700

    wip

--- src/servo/css/select_handler.rs
+++ src/servo/css/select_handler.rs
@@ -1,5 +1,6 @@
-use dom::node::{Node, Doctype, Comment, Element, Text};
+use dom::node::{Node, NodeTree, Doctype, Comment, Element, Text};
 use newcss::SelectHandler;
+use util::tree;
 
 pub struct NodeSelectHandler {
     node: Node
@@ -20,5 +21,25 @@ impl NodeSelectHandler: SelectHandler<Node> {
                 Text(*) => unnamed_node("text")
             }
         }
+    }
+
+    fn named_parent_node(node: &Node, name: &str) -> Option<Node> {
+        let parent = tree::parent(&NodeTree, node);
+        match parent {
+            Some(parent) => {
+                do parent.read |data| {
+                    if name == data.tag_name {
+                        Some(parent)
+                    } else {
+                        None
+                    }
+                }
+            }
+            None => None
+        }
+    }
+
+    fn parent_node(node: &Node) -> Option<Node> {
+        tree::parent(&NodeTree, node)
     }
 }
